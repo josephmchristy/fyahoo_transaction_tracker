e5217a8ad5fa8478547642359802291a4440b111
josephmchristy <josephmchristy@gmail.com>
2021-01-26 23:20:52 -0800

    Added email functionality.

--- send_transactions.py
+++ send_transactions.py
@@ -2,6 +2,7 @@
 
 import os
 import ffyahoo
+import gmail
 from datetime import datetime
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
@@ -20,12 +21,30 @@ with open('last_transaction_date.txt', 'r') as last_trans:
     prev_date = int(prev_date[0])
     prev_date = datetime.fromtimestamp(prev_date)
 
+msgPlain = ''
 # Print transactions following last transaction date
 for transaction in transactions_list:
     if transaction['date'] > prev_date:
-        print(transaction)
+        if transaction['type'] == 'add':
+            ttype = 'added'
+        elif transaction['type'] == 'drop':
+            ttype = 'dropped'
+        towner = transaction['owner']
+        tplayer = transaction['player']
+        tdate = transaction['date']
+        tmsg = '{} {} {} at {}\n'.format(towner, ttype, tplayer, tdate)
+        msgPlain += tmsg
 
-# Store the current last transaction date
-curr_date = datetime.timestamp(list(reversed(transactions_list))[-1]['date'])
-with open('last_transaction_date.txt', 'w') as last_trans:
-    print(curr_date, file=last_trans)
+print (msgPlain)
+
+# Send the new transactions, if there are any
+if msgPlain != '':
+    to = "josephmchristy@gmail.com"
+    sender = "josephmchristy@gmail.com.com"
+    subject = "subject"
+    gmail.SendMessage(sender, to, subject, msgHtml=None, msgPlain=msgPlain)
+    # Store the current last transaction date
+    curr_date = datetime.timestamp(list(reversed(transactions_list))[-1]['date'])
+    with open('last_transaction_date.txt', 'w') as last_trans:
+        print(curr_date, file=last_trans)
+
